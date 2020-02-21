eecad353121a93f994a295c318d32eaa8d68f9a2
Чунаев Михаил Михайлович <chunaevmm@askona.ru>
2020-02-21 17:25:54 +0300

    сделал десктопное меню

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+console.log("it actually works!");
+
+// Подменю
+
+$('.open-submenu-js').click(function() {
+    $('.header-catalogue-sub').toggleClass('is-opened-js');
+})
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const autoprefix = require('gulp-autoprefixer');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+const paths = {
+    html:['header.html'],
+    css:['./src/scss/vars.scss', './src/scss/common.scss', './src/scss/header.scss'],
+    js:['./src/js/main.js']
+  };
+
+// CSS task
+
+gulp.task('css', function () {
+    return gulp.src(paths.css)
+    .pipe(concat('style.scss'))
+    .pipe(sass())
+    .pipe(autoprefix())
+     // .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
+// JS task
+
+gulp.task('js', function () {
+    return gulp.src(paths.js)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(browserSync.stream());
+});
+
+// Cleaning dist task
+
+gulp.task('del', function() {
+  return del(['dist/css/*', 'dist/js/*']);
+});
+
+// Watcher
+
+function watch() {
+  browserSync.init({
+     server: {
+         baseDir: "./",
+         index: "header.html"
+     }
+ });
+ //Watch CSS files
+ gulp.watch('./src/scss/**/*.scss').on('change', gulp.series('css', browserSync.reload))
+ //Watch JS files
+ gulp.watch('./src/js/**/*.js').on('change', gulp.series('js', browserSync.reload))
+ //Watch HTML files
+ gulp.watch("./*.html").on('change', browserSync.reload)
+}
+
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.series('del', gulp.parallel('css', 'js', 'watch')));
