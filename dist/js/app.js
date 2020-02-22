7081d57df89d42641f3c2f207106c1ebcb9af559
Samgeven <sam.geven@mail.ru>
2020-02-23 02:08:02 +0300

    сделал хэдер

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,3 +5,23 @@ console.log("it actually works!");
 $('.open-submenu-js').click(function() {
     $('.header-catalogue-sub').toggleClass('is-opened-js');
 })
+/*
+$('.open-submenu-js').on('focusout', function () {
+    $('.header-catalogue-sub').removeClass('is-opened-js');
+});
+*/
+
+$('.menu-back-js').click(function() {
+	$('.header-catalogue-sub').toggleClass('is-opened-js');
+})
+
+// Мобильное меню
+
+$(function(){
+	$('.js-menu-trigger').click(function(){
+		$('body').toggleClass('menu-opened'); // toggling mob menu on hamburger
+	});
+	$('.overlay').click(function(){
+		$('body').removeClass('menu-opened'); // removing sidebar on click outside
+    })
+});
--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const browserSync = require('browser-sync').create();
 const paths = {
     html:['header.html'],
     css:['./src/scss/vars.scss', './src/scss/common.scss', './src/scss/header.scss'],
+    media:['./src/scss/vars.scss', './src/scss/media-1200.scss', './src/scss/media-992.scss',  './src/scss/media-768.scss', './src/scss/media-576.scss'],
     js:['./src/js/main.js']
   };
 
@@ -22,6 +23,16 @@ gulp.task('css', function () {
      // .pipe(cleanCSS())
     .pipe(gulp.dest('./dist/css'))
     .pipe(browserSync.stream());
+});
+
+gulp.task('media', function () {
+  return gulp.src(paths.media)
+  .pipe(concat('media.scss'))
+  .pipe(sass())
+  .pipe(autoprefix())
+   // .pipe(cleanCSS())
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(browserSync.stream());
 });
 
 // JS task
@@ -50,6 +61,7 @@ function watch() {
  });
  //Watch CSS files
  gulp.watch('./src/scss/**/*.scss').on('change', gulp.series('css', browserSync.reload))
+ gulp.watch('./src/scss/**/*.scss').on('change', gulp.series('media', browserSync.reload))
  //Watch JS files
  gulp.watch('./src/js/**/*.js').on('change', gulp.series('js', browserSync.reload))
  //Watch HTML files
@@ -58,4 +70,4 @@ function watch() {
 
 gulp.task('watch', watch);
 
-gulp.task('default', gulp.series('del', gulp.parallel('css', 'js', 'watch')));
+gulp.task('default', gulp.series('del', gulp.parallel('css', 'media', 'js', 'watch')));
